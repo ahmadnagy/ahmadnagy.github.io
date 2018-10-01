1f554ef3fe33858e9a2d43c27a2f8580ec4d10ba
AhmadNagy <ahmad.ios.tweaks@gmail.com>
2018-10-01 14:42:58 +0400

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 dpkg-deb -bZgzip Projects/AllByeTweaks debs
 # dpkg-deb --bZgzip Projects/<projectname> <output folder>
+
+#!/bin/bash
+dpkg-deb -bZgzip Projects/ZoopProbeta debs
+# dpkg-deb --bZgzip Projects/<projectname> <output folder>
