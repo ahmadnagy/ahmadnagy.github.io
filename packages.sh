d3fe6cc608d47c7518de4823e8b041fda736a3af
AhmadNagy <ahmad.ios.tweaks@gmail.com>
2018-10-05 03:43:52 +0400

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 dpkg-deb -bZgzip Projects/AllByeTweaks debs
-# dpkg-deb --bZgzip Projects/<projectname> <output folder>
 
 #!/bin/bash
 dpkg-deb -bZgzip Projects/ZoopProbeta debs
-# dpkg-deb --bZgzip Projects/<projectname> <output folder>
+
+#!/bin/bash
+dpkg-deb -bZgzip Projects/NormalWifiIcon debs
+
+#!/bin/bash
+dpkg-deb -bZgzip Projects/Kasper debs
